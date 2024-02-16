5447dc3a7182bd7c3e955d51cc7184710faf594b
Ricardo Lima <ricardolima.dev@gmail.com>
2024-02-16 13:58:09 -0300

    conifg adjust to deploy

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/desafio-pitang'));
+app.use(express.static(__dirname + '/dist/desafio-pitang/browser'));
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/desafio-pitang/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/desafio-pitang/browser/index.html'));
 })
 
 app.listen(process.env.PORT || 4200)
